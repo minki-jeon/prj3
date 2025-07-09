d39f058d2058fe29bb8263d4d3789ffdee74d4f2
minki-jeon <minki.jeon.study@gmail.com>
2025-07-09 11:19:40 +0900

    Navbar NavLink (해당 컴포넌트 내에서만)

--- frontend/src/common/AppNavBar.jsx
+++ frontend/src/common/AppNavBar.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router";
+import { Link, NavLink } from "react-router";
 import { Container, Nav, Navbar } from "react-bootstrap";
 
 export function AppNavBar() {
@@ -10,8 +10,12 @@ export function AppNavBar() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="/">HOME</Nav.Link>
-              <Nav.Link href="/board/add">글작성</Nav.Link>
+              <Nav.Link as={NavLink} to="/">
+                HOME
+              </Nav.Link>
+              <Nav.Link as={NavLink} to="/board/add">
+                글작성
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
