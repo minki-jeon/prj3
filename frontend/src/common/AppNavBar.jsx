91184792ef679a5f3a36193e2535de4a1274a1b9
minki-jeon <minki.jeon.study@gmail.com>
2025-07-09 11:16:56 +0900

    Navbar 추가, 부트스트랩 적용

--- frontend/src/common/AppNavBar.jsx
+++ frontend/src/common/AppNavBar.jsx
@@ -1,11 +1,21 @@
 import { Link } from "react-router";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 export function AppNavBar() {
   return (
     <div>
-      navbar
-      <Link to="/">HOME</Link>
-      <Link to="/board/add">글쓰기</Link>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand>PRJ3</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/">HOME</Nav.Link>
+              <Nav.Link href="/board/add">글작성</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   );
 }
