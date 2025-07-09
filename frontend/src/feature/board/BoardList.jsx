38951723cd0336492f31f8f7fdf82444bd0c95b7
minki-jeon <minki.jeon.study@gmail.com>
2025-07-09 11:56:25 +0900

    글목록 controller 매핑

--- frontend/src/feature/board/BoardList.jsx
+++ frontend/src/feature/board/BoardList.jsx
@@ -1,4 +1,21 @@
+import { useEffect } from "react";
+import axios from "axios";
+
 export function BoardList() {
+  useEffect(() => {
+    // 마운트될 때 (initial render 시) 실행되는 코드
+    axios
+      .get("/api/board/list")
+      .then((res) => {
+        console.log("동작 성공");
+      })
+      .catch((err) => {
+        console.log("오류 발생");
+      })
+      .finally(() => {
+        console.log("항상 실행");
+      });
+  }, []);
   return (
     <div>
       <h3>글 목록</h3>
