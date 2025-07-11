691b052e0ef21add6e67506e7c3a17e0e47c1f4d
minki-jeon <minki.jeon.study@gmail.com>
2025-07-11 17:11:47 +0900

    로그인 여부에 따른 적절한 navLink 항목 출력

--- frontend/src/common/AppNavBar.jsx
+++ frontend/src/common/AppNavBar.jsx
@@ -1,7 +1,11 @@
 import { Link, NavLink } from "react-router";
 import { Container, Nav, Navbar } from "react-bootstrap";
+import { AuthenticationContext } from "./AuthenticationContextProvider.jsx";
+import { useContext } from "react";
 
 export function AppNavBar() {
+  const { user } = useContext(AuthenticationContext);
+
   return (
     <div>
       <Navbar expand="lg" className="bg-body-tertiary">
@@ -15,21 +19,36 @@ export function AppNavBar() {
               <Nav.Link as={NavLink} to="/">
                 HOME
               </Nav.Link>
-              <Nav.Link as={NavLink} to="/board/add">
-                글작성
-              </Nav.Link>
-              <Nav.Link as={NavLink} to="/signup">
-                가입
-              </Nav.Link>
-              <Nav.Link as={NavLink} to="/member/list">
-                회원목록
-              </Nav.Link>
-              <Nav.Link as={NavLink} to="/login">
-                로그인
-              </Nav.Link>
-              <Nav.Link as={NavLink} to="/logout">
-                로그아웃
-              </Nav.Link>
+              {user !== null && (
+                <Nav.Link as={NavLink} to="/board/add">
+                  글작성
+                </Nav.Link>
+              )}
+              {user === null && (
+                <Nav.Link as={NavLink} to="/signup">
+                  가입
+                </Nav.Link>
+              )}
+              {user !== null && (
+                <Nav.Link as={NavLink} to="/member/list">
+                  회원목록
+                </Nav.Link>
+              )}
+              {user === null && (
+                <Nav.Link as={NavLink} to="/login">
+                  로그인
+                </Nav.Link>
+              )}
+              {user !== null && (
+                <Nav.Link as={NavLink} to="/logout">
+                  로그아웃
+                </Nav.Link>
+              )}
+              {user !== null && (
+                <Nav.Link as={NavLink} to={`/member?email=${user.email}`}>
+                  {user.nickName}
+                </Nav.Link>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
