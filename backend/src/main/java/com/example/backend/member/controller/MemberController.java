0a3d79d7aa89d593b827554fc02e738ac69d49ae
minki-jeon <minki.jeon.study@gmail.com>
2025-07-11 15:14:33 +0900

    로그인 처리 / jwt 토큰

--- backend/src/main/java/com/example/backend/member/controller/MemberController.java
+++ backend/src/main/java/com/example/backend/member/controller/MemberController.java
@@ -18,10 +18,19 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("login")
-    public String login(@RequestBody MemberLoginForm loginForm) {
-        System.out.println(loginForm);
+    public ResponseEntity<?> login(@RequestBody MemberLoginForm loginForm) {
+//        System.out.println(loginForm);
 
-        return null;
+        try {
+            String token = memberService.getToken(loginForm);
+            return ResponseEntity.ok().body(Map.of("token", token, "message",
+                    Map.of("type", "success", "text", "로그인을 성공하였습니다.")));
+        } catch (Exception e) {
+            e.printStackTrace();
+            String message = e.getMessage();
+            return ResponseEntity.status(401).body(Map.of("message",
+                    Map.of("type", "error", "text", message)));
+        }
     }
 
     @PutMapping("changePassword")
