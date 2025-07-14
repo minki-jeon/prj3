56c49ae7d385399edcbac878c10bfecd0ed032e2
minki-jeon <minki.jeon.study@gmail.com>
2025-07-14 16:59:31 +0900

    게시글 목록/상세 데이터조회 - 회원데이터 참조, 검색+페이지 조건

--- backend/src/main/java/com/example/backend/board/dto/BoardDto.java
+++ backend/src/main/java/com/example/backend/board/dto/BoardDto.java
@@ -1,15 +1,21 @@
 package com.example.backend.board.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BoardDto {
     private Integer id;
     private String title;
     private String content;
-    private String author;
+    //    private String author;
+    private String authorNickName;
+    private String authorEmail;
     private LocalDateTime insertedAt;
 
 }
--- backend/src/main/java/com/example/backend/board/repository/BoardRepository.java
+++ backend/src/main/java/com/example/backend/board/repository/BoardRepository.java
@@ -1,11 +1,51 @@
 package com.example.backend.board.repository;
 
+import com.example.backend.board.dto.BoardDto;
 import com.example.backend.board.dto.BoardListInfo;
 import com.example.backend.board.entity.Board;
+import com.example.backend.member.dto.BoardListDto;
+import com.example.backend.member.entity.Member;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface BoardRepository extends JpaRepository<Board, Integer> {
     List<BoardListInfo> findAllByOrderByIdDesc();
+
+    @Query(value = """
+            SELECT new com.example.backend.member.dto.BoardListDto(
+                        b.id,
+                        b.title,
+                        m.nickName,
+                        b.insertedAt
+                        )
+            FROM Board b JOIN Member m
+                    ON b.author.email = m.email
+            WHERE b.title LIKE %:keyword%
+                OR b.content LIKE %:keyword%
+                OR m.nickName LIKE %:keyword%
+            ORDER BY b.id DESC
+            """)
+//    List<BoardListDto> findAllBy(String keyword);
+    Page<BoardListDto> findAllBy(String keyword, PageRequest pageRequest);
+
+    @Query(value = """
+            SELECT new com.example.backend.board.dto.BoardDto(
+                        b.id,
+                        b.title,
+                        b.content,
+                        m.nickName authorNickName,
+                        m.email authorEmail,
+                        b.insertedAt
+                        )
+                FROM Board b JOIN Member m
+                    ON b.author.email = m.email
+                WHERE b.id = :id
+            """)
+    BoardDto findBoardById(Integer id);
+
+    void deleteByAuthor(Member dbData);
 }
