b86cf2a1efe001781980e8ac91123e61e4b8c0d4
minki-jeon <minki.jeon.study@gmail.com>
2025-07-15 10:33:28 +0900

    댓글 목록 조회 처리

---
+++ backend/src/main/java/com/example/backend/comment/dto/CommentListDto.java
@@ -0,0 +1,50 @@
+package com.example.backend.comment.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentListDto {
+    private Integer id;
+    private Integer boardId;
+    private String authorNickName;
+    private String comment;
+    private LocalDateTime insertedAt;
+
+    public String getTimesAgo() {
+        LocalDateTime now = LocalDateTime.now(ZoneId.of("Asia/Seoul"));
+        LocalDateTime insertedAt = this.getInsertedAt();
+
+        Duration duration = Duration.between(insertedAt, now);
+
+        long seconds = duration.toSeconds();
+
+        if (seconds < 60) {                 // 1분 이내
+            return "방금 전";
+        } else if (seconds < 3600) {        // 1시간 이내
+            long minutes = seconds / 60;
+            return minutes + " 분 전";
+        } else if (seconds < 60 * 60 * 24) {       // 1일 이내
+            long hours = seconds / 3600;
+            return hours + " 시간 전";
+        } else if (seconds < 3600 * 24 * 7) {       // 1주일
+            long days = seconds / 3600 / 24;
+            return days + " 일 전";
+        } else if (seconds < 3600 * 24 * 7 * 4) {       // 4주 이내
+            long weeks = seconds / 3600 / 24 * 7;
+            return weeks + " 주 전";
+        } else {
+            long days = duration.toDays();
+            long years = days / 365;
+            return years + " 년 전";
+        }
+    }
+
+}
--- backend/src/main/java/com/example/backend/comment/service/CommentService.java
+++ backend/src/main/java/com/example/backend/comment/service/CommentService.java
@@ -3,6 +3,7 @@ package com.example.backend.comment.service;
 import com.example.backend.board.entity.Board;
 import com.example.backend.board.repository.BoardRepository;
 import com.example.backend.comment.dto.CommentForm;
+import com.example.backend.comment.dto.CommentListDto;
 import com.example.backend.comment.entity.Comment;
 import com.example.backend.comment.repository.CommentRepository;
 import com.example.backend.member.entity.Member;
@@ -11,6 +12,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -38,5 +41,10 @@ public class CommentService {
         data.setAuthor(member);
 
         commentRepository.save(data);
+    }
+
+    public List<CommentListDto> listByBoardId(Integer boardId) {
+
+        return commentRepository.listByBoardId(boardId);
     }
 }
