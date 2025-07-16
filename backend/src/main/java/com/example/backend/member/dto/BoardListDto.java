0b5cd8b5a629413ceca63ef9f9b8f2e8f0759e74
minki-jeon <minki.jeon.study@gmail.com>
2025-07-16 11:19:28 +0900

    게시물 목록 좋아요 수 조회 (DTO, Query 수정)

--- backend/src/main/java/com/example/backend/member/dto/BoardListDto.java
+++ backend/src/main/java/com/example/backend/member/dto/BoardListDto.java
@@ -18,6 +18,7 @@ public class BoardListDto {
     private String nickName;
     private LocalDateTime insertedAt;
     private Long countComment;
+    private Long countLike;
 
     public String getTimesAgo() {
         LocalDateTime now = LocalDateTime.now(ZoneId.of("Asia/Seoul"));
