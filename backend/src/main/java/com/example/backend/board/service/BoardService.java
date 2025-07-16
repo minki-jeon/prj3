4798dbcc50f8ed6dcc1f4b787c91e231fc2be691
minki-jeon <minki.jeon.study@gmail.com>
2025-07-16 12:48:26 +0900

    파일 업로드 처리 - db기록+file저장

--- backend/src/main/java/com/example/backend/board/service/BoardService.java
+++ backend/src/main/java/com/example/backend/board/service/BoardService.java
@@ -1,5 +1,9 @@
 package com.example.backend.board.service;
 
+import com.example.backend.board.dto.BoardAddForm;
+import com.example.backend.board.entity.BoardFile;
+import com.example.backend.board.entity.BoardFileId;
+import com.example.backend.board.repository.BoardFileRepository;
 import com.example.backend.board.repository.BoardRepository;
 import com.example.backend.board.dto.BoardDto;
 import com.example.backend.board.entity.Board;
@@ -13,7 +17,12 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.List;
 import java.util.Map;
 
@@ -25,8 +34,9 @@ public class BoardService {
     private final BoardRepository boardRepository;
     private final MemberRepository memberRepository;
     private final CommentRepository commentRepository;
+    private final BoardFileRepository boardFileRepository;
 
-    public void add(BoardDto dto, Authentication authentication) {
+    public void add(BoardAddForm dto, Authentication authentication) {
         if (authentication == null) {
             throw new RuntimeException("권한이 없습니다.");
         }
@@ -43,7 +53,59 @@ public class BoardService {
         // Repository에서 Save 실행
         boardRepository.save(board);
 
+        // files save
+        saveFiles(board, dto);
 
+    }
+
+    private void saveFiles(Board board, BoardAddForm dto) {
+        List<MultipartFile> files = dto.getFiles();
+        if (files != null && files.size() > 0) {
+            for (MultipartFile file : files) {
+                if (file != null && file.getSize() > 0) {
+                    // board_file 테이블 새 레코드 입력
+                    BoardFile boardFile = new BoardFile();
+                    // entity set
+                    BoardFileId id = new BoardFileId();
+                    id.setBoardId(board.getId());
+                    id.setName(file.getOriginalFilename());
+                    boardFile.setBoard(board);
+                    boardFile.setId(id);
+
+                    // repository
+                    boardFileRepository.save(boardFile);
+
+                    // 실제 파일 server(local) disk 저장
+                    // TODO : AWS S3 에 파일 저장
+                    // 1) ../Temp/prj3/boardFile 에서 '게시물 번호'이름의 폴더 생성
+                    File folder = new File("D:/01.private_work/Choongang/workspaces/Temp/prj3/boardFile/" + board.getId());
+                    if (!folder.exists()) {
+                        folder.mkdirs();
+                    }
+
+                    // 2) 폴더에 파일 저장
+                    try {
+                        BufferedInputStream bi = new BufferedInputStream(file.getInputStream());
+                        BufferedOutputStream bo = new BufferedOutputStream(new FileOutputStream(new File(folder, file.getOriginalFilename())));
+                        try (bi; bo) {
+                            byte[] b = new byte[1024];
+                            int len;
+                            while ((len = bi.read(b)) != -1) {
+                                bo.write(b, 0, len);
+                            }
+                            bo.flush();
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        throw new RuntimeException(e);
+                    }
+
+
+                }
+            }
+
+
+        }
     }
 
     public boolean validate(BoardDto dto) {
@@ -125,5 +187,16 @@ public class BoardService {
             throw new RuntimeException("권한이 없습니다.");
         }
 
+    }
+
+    public boolean validateForAdd(BoardAddForm dto) {
+        if (dto.getTitle() == null || dto.getTitle().trim().isBlank()) {
+            return false;
+        }
+        if (dto.getContent() == null || dto.getContent().trim().isBlank()) {
+            return false;
+        }
+
+        return true;
     }
 }
