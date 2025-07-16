1ed65e390f025885c2bf7648c609fad988634b79
minki-jeon <minki.jeon.study@gmail.com>
2025-07-16 09:47:10 +0900

    좋아요 버튼 처리

--- frontend/src/feature/like/LikeContainer.jsx
+++ frontend/src/feature/like/LikeContainer.jsx
@@ -1,9 +1,24 @@
 import { FaRegThumbsUp, FaThumbsUp } from "react-icons/fa6";
+import axios from "axios";
+import { useState } from "react";
 
-export function LikeContainer() {
+export function LikeContainer({ boardId }) {
+  const [isProcessing, setIsProcessing] = useState(false);
+
+  function handleLikeClick() {
+    setIsProcessing(true);
+    axios
+      .put("/api/like", { boardId: boardId })
+      .then((res) => {})
+      .catch((err) => {})
+      .finally(() => {
+        setIsProcessing(false);
+      });
+  }
+
   return (
     <div className="d-flex gap-2 h2">
-      <div>
+      <div onClick={handleLikeClick}>
         <FaRegThumbsUp />
         <FaThumbsUp />
       </div>
